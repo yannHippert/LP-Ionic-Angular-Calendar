5f644ec66c646744b744a3fdade3f221ad364207
Yann Hippert <yann.hippert@hotmail.com>
2023-03-17 19:02:20 +0100

    Local storage?

--- src/app/services/database/database.service.ts
+++ src/app/services/database/database.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { AlertController } from '@ionic/angular';
-import { BehaviorSubject, from, of } from 'rxjs';
+import { BehaviorSubject, from, Observable, of } from 'rxjs';
 import { switchMap } from 'rxjs/operators';
 import '@capacitor-community/sqlite';
 import { JsonSQLite } from '@capacitor-community/sqlite';
@@ -10,10 +10,10 @@ import { EventService } from '@services/event/event.service';
 import { IEvent } from '@models/event.model';
 const { CapacitorSQLite, Device, Storage } = Plugins;
 
-const DB_SETUP_KEY = 'events_db_setup';
-const DB_NAME_KEY = 'dbEvents';
+import DB_SCHEMA from './schema.json';
 
 // https://devdactic.com/sqlite-ionic-app-with-capacitor
+// https://github.com/capacitor-community/sqlite/blob/master/docs/Ionic-Angular-Usage.md
 
 @Injectable({
   providedIn: 'root',
@@ -25,7 +25,9 @@ export class DatabaseService {
   constructor(
     private alertCtrl: AlertController,
     private EventService: EventService
-  ) {}
+  ) {
+    this.dbName = DB_SCHEMA.database;
+  }
 
   async init(): Promise<void> {
     const info = await Device['getInfo']();
@@ -49,43 +51,73 @@ export class DatabaseService {
   }
 
   private async setupDatabase() {
-    const dbSetupDone = await Storage['get']({ key: DB_SETUP_KEY });
+    console.log('Setting up database');
+    const dbSetupDone = await Storage['get']({ key: this.dbName });
 
-    if (!dbSetupDone.value) {
-      this.downloadDatabase();
+    if (!dbSetupDone) {
+      this.createDatabase();
     } else {
-      this.dbName = (await Storage['get']({ key: DB_NAME_KEY })).value;
       await CapacitorSQLite['open']({ database: this.dbName });
       this.dbReady.next(true);
+      this.updateDatabase();
     }
   }
 
-  private updateDatabaseData() {
-    this.EventService.getAll().subscribe((events: Array<IEvent>) => {});
-  }
-  private downloadDatabase(update = false) {
-    this.http
-      .get('https://devdactic.fra1.digitaloceanspaces.com/tutorial/db.json')
-      .subscribe(async (jsonExport: JsonSQLite) => {
-        const jsonstring = JSON.stringify(jsonExport);
-        const isValid = await CapacitorSQLite.isJsonValid({ jsonstring });
-
-        if (isValid.result) {
-          this.dbName = jsonExport.database;
-          await Storage.set({ key: DB_NAME_KEY, value: this.dbName });
-          await CapacitorSQLite.importFromJson({ jsonstring });
-          await Storage.set({ key: DB_SETUP_KEY, value: '1' });
-
-          // Your potential logic to detect offline changes later
-          if (!update) {
-            await CapacitorSQLite.createSyncTable();
-          } else {
-            await CapacitorSQLite.setSyncDate({
-              syncdate: '' + new Date().getTime(),
-            });
-          }
-          this.dbReady.next(true);
-        }
+  private async createDatabase() {
+    console.log('Creating database');
+    const isSetup = await Storage['get']({ key: this.dbName });
+    if (!isSetup) {
+      const isValid = await CapacitorSQLite['isJsonValid']({
+        jsonstring: JSON.stringify(DB_SCHEMA),
       });
+      if (isValid.result) {
+        await CapacitorSQLite['importFromJson']({ jsonstring: DB_SCHEMA });
+        await Storage['set']({ key: this.dbName, value: '1' });
+        console.log('Database created');
+      }
+    }
+    this.dbReady.next(true);
+    this.updateDatabase();
+  }
+
+  private async updateDatabase() {
+    console.log('Update database call');
+    this.dbReady.subscribe((isReady) => {
+      if (isReady) {
+        this.EventService.getAll().subscribe((events: Array<IEvent>) => {
+          console.log('Event updates -> Updateing database');
+          const statement = `TRUNCATE TABLE tblEvents;`;
+          CapacitorSQLite['query']({ statement, values: [] });
+        });
+      }
+    });
+  }
+
+  getEvents(): Observable<any> {
+    console.log('Retrieving events from database');
+    return this.dbReady.pipe(
+      switchMap((isReady) => {
+        if (!isReady) {
+          return of({ values: [] });
+        } else {
+          const statement = 'SELECT * FROM tblEvents;';
+          return from(CapacitorSQLite['query']({ statement, values: [] }));
+        }
+      })
+    );
+  }
+
+  getEventById(eventId: string) {
+    console.log(`Retrieving event with id ${eventId} from database`);
+    return this.dbReady.pipe(
+      switchMap((isReady) => {
+        if (!isReady) {
+          return of({ values: [] });
+        } else {
+          const statement = `SELECT * FROM tblEvents WHERE idEvent=${eventId};`;
+          return from(CapacitorSQLite['query']({ statement, values: [] }));
+        }
+      })
+    );
   }
 }
