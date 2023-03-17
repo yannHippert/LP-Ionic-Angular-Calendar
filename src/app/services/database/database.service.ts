5c5411c5d0768f674741f2b55f07a2cf8158a8d3
Yann Hippert <yann.hippert@hotmail.com>
2023-03-17 12:02:58 +0100

    Start of local database

--- src/app/services/database/database.service.ts
+++ src/app/services/database/database.service.ts
@@ -1,49 +1,91 @@
 import { Injectable } from '@angular/core';
-import { SQLite, SQLiteObject } from '@awesome-cordova-plugins/sqlite/ngx';
-import { IEvent } from '@models/event.model';
+import { AlertController } from '@ionic/angular';
+import { BehaviorSubject, from, of } from 'rxjs';
+import { switchMap } from 'rxjs/operators';
+import '@capacitor-community/sqlite';
+import { JsonSQLite } from '@capacitor-community/sqlite';
+
+import { Plugins } from '@capacitor/core';
 import { EventService } from '@services/event/event.service';
-import { BehaviorSubject } from 'rxjs';
+import { IEvent } from '@models/event.model';
+const { CapacitorSQLite, Device, Storage } = Plugins;
+
+const DB_SETUP_KEY = 'events_db_setup';
+const DB_NAME_KEY = 'dbEvents';
+
+// https://devdactic.com/sqlite-ionic-app-with-capacitor
 
 @Injectable({
   providedIn: 'root',
 })
 export class DatabaseService {
-  private static dbName = 'data.dbCalendar';
+  dbReady = new BehaviorSubject(false);
+  dbName = '';
 
-  private database: SQLiteObject | undefined = undefined;
-  public databaseReady = new BehaviorSubject<boolean>(false);
+  constructor(
+    private alertCtrl: AlertController,
+    private EventService: EventService
+  ) {}
 
-  constructor(private sqlite: SQLite, private EventService: EventService) {
-    this.sqlite
-      .create({
-        name: DatabaseService.dbName,
-        location: 'default',
-      })
-      .then((db: SQLiteObject) => {
-        this.database = db;
-        this.createTable();
-        this.databaseReady.next(true);
+  async init(): Promise<void> {
+    const info = await Device['getInfo']();
 
-        EventService.getAll().subscribe((events: Array<IEvent>) => {});
-      })
-      .catch((e) => console.log(e));
+    if (info.platform === 'android') {
+      try {
+        const sqlite = CapacitorSQLite as any;
+        await sqlite.requestPermissions();
+        this.setupDatabase();
+      } catch (e) {
+        const alert = await this.alertCtrl.create({
+          header: 'No DB access',
+          message: 'Database access is required for offline functionality.',
+          buttons: ['OK'],
+        });
+        await alert.present();
+      }
+    } else {
+      this.setupDatabase();
+    }
   }
 
-  createTable() {
-    this.database?.executeSql(
-      `CREATE TABLE IF NOT EXISTS tblEvent(
-        id VARCHAR(255) NOT NULL PRIMARY KEY,
-        name VARCHAR(255) NOT NULL,
-        startDate UNSIGNED BIGINT NOT NULL,
-        endData UNSIGNED BIGINT NOT NULL,
-        color VARCHAR(8) NULL,
-      )`
-    );
+  private async setupDatabase() {
+    const dbSetupDone = await Storage['get']({ key: DB_SETUP_KEY });
+
+    if (!dbSetupDone.value) {
+      this.downloadDatabase();
+    } else {
+      this.dbName = (await Storage['get']({ key: DB_NAME_KEY })).value;
+      await CapacitorSQLite['open']({ database: this.dbName });
+      this.dbReady.next(true);
+    }
   }
 
-  getAllEvents() {
-    this.database?.executeSql('SELECT * FROM tblEvent').then((sqlEvents) => {
-      console.log(sqlEvents);
-    });
+  private updateDatabaseData() {
+    this.EventService.getAll().subscribe((events: Array<IEvent>) => {});
+  }
+  private downloadDatabase(update = false) {
+    this.http
+      .get('https://devdactic.fra1.digitaloceanspaces.com/tutorial/db.json')
+      .subscribe(async (jsonExport: JsonSQLite) => {
+        const jsonstring = JSON.stringify(jsonExport);
+        const isValid = await CapacitorSQLite.isJsonValid({ jsonstring });
+
+        if (isValid.result) {
+          this.dbName = jsonExport.database;
+          await Storage.set({ key: DB_NAME_KEY, value: this.dbName });
+          await CapacitorSQLite.importFromJson({ jsonstring });
+          await Storage.set({ key: DB_SETUP_KEY, value: '1' });
+
+          // Your potential logic to detect offline changes later
+          if (!update) {
+            await CapacitorSQLite.createSyncTable();
+          } else {
+            await CapacitorSQLite.setSyncDate({
+              syncdate: '' + new Date().getTime(),
+            });
+          }
+          this.dbReady.next(true);
+        }
+      });
   }
 }
